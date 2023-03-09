09867465117ac07c790e5a41b6050c2dae6981bd
JMGR21 <jtutosgr@gmail.com>
2023-03-09 15:20:26 -0600

    sdjkhad

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3030;
 
 app.get('/', (req, res) => {
-  res.send('connection opened');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
